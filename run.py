720366966f7e2af7fd89f91f1aeda5138d0dd2b0
Evgeniy Tikhonov <evgeniy.tikhonov@bostongene.com>
2024-01-27 22:33:52 +0400

    Enable download file from sra & convert sra to fastq

--- run.py
+++ run.py
@@ -46,6 +46,87 @@ def parse_args() -> dict:
     return vars(parser.parse_args())
 
 
+def exit_with_error(message: str):
+    logger.critical(message)
+    sys.exit(1)
+
+
+def compress_fastq_file(file_path: str, compressed_file_path: str):
+    logger.info(f"Compressing '{file_path}' with pigz...")
+    try:
+        with open(compressed_file_path, "w") as f_out:
+            cmd = ["pigz", "-c", file_path]
+            logger.info(f"Running the following command: '{' '.join(cmd)}'")
+            subprocess.run(cmd, stdout=f_out)
+            logger.info(f"Compressed '{file_path}' to '{compressed_file_path}' successfully.")
+    except Exception as error:
+        exit_with_error(f"Failed to compress '{file_path}': {error}")
+
+
+def compress_existing_fastq_files(temp_output_mapping: dict) -> None:
+    for temp_fastq, output_fastq_gz in temp_output_mapping.items():
+        if check_file_exists(temp_fastq) and output_fastq_gz is not None:
+            compress_fastq_file(temp_fastq, output_fastq_gz)
+        elif check_file_exists(temp_fastq) and output_fastq_gz is None:
+            exit_with_error(f"File '{temp_fastq}' is not empty, but output file name is not specified.")
+        else:
+            logger.info(f"File '{temp_fastq}' is empty or does not exist, skipping...")
+
+
+def check_file_exists(filename: str):
+    return os.path.exists(filename) and os.path.getsize(filename) > 0
+
+
+def convert_sra_to_fastq(sra_id: str, ngc_key_file: Optional[str]) -> None:
+    logger.info(f"Converting '{sra_id}.sra' to fastq file...")
+    cmd = ["fasterq-dump", sra_id, "-e", str(cpu_count()), "-O", tempdir, "-x"]
+    if ngc_key_file:
+        cmd += ["--ngc", ngc_key_file]
+    logger.info(f"Running the following command: '{' '.join(cmd)}'")
+    result = subprocess.run(cmd)
+    if result.returncode != 0:
+        exit_with_error(f"Failed to convert '{sra_id}.sra' to fastq file")
+    logger.info(f"Successfully converted '{sra_id}.sra' to '.fastq' file(s)")
+
+
+def validate_sra_file(sra_id: str, ngc_arg: List) -> subprocess.CompletedProcess:
+    logger.info(f"Validating '{sra_id}' accession...")
+    sra_file_path = os.path.join(tempdir, sra_id, f"{sra_id}.sra")
+    cmd = ["vdb-validate", sra_file_path] + ngc_arg
+    logger.info(f"Running the following command with subprocess.run: '{' '.join(cmd)}'")
+    result = subprocess.run(cmd)
+    return result
+
+
+def download_sra_file(sra_id: str, ngc_key_file: Optional[str]) -> Optional[int]:
+    ngc_arg = ["--ngc", ngc_key_file] if ngc_key_file else []
+    for i in range(DOWNLOAD_SRA_CONNECTIONS):
+        logger.info(f"Try #{i + 1}: downloading '{sra_id}' accession...")
+        cmd = ["prefetch", sra_id] + ngc_arg + ["--max-size", "u"]
+        logger.info(f"Running the following command with subprocess.run: '{' '.join(cmd)}'")
+        subprocess.run(cmd)
+        sra_download_check = validate_sra_file(sra_id, ngc_arg).returncode
+        if sra_download_check == 0:
+            logger.info(f"'{sra_id}' accession is downloaded successfully.")
+            break
+    return sra_download_check
+
+
+def run(sra_id: str,
+        ngc_key_file: Optional[str],
+        output_forward_fastq_gz: Optional[str],
+        output_reverse_fastq_gz: Optional[str],
+        output_single_fastq_gz: Optional[str]) -> None:
+
+    os.chdir(tempdir)
+    download_sra_file(sra_id, ngc_key_file)
+    convert_sra_to_fastq(sra_id, ngc_key_file)
+    temp_output_mapping = {
+        os.path.join(tempdir, f"{sra_id}_1.fastq"): output_forward_fastq_gz,
+        os.path.join(tempdir, f"{sra_id}_2.fastq"): output_reverse_fastq_gz,
+        os.path.join(tempdir, f"{sra_id}.fastq"): output_single_fastq_gz,
+    }
+    compress_existing_fastq_files(temp_output_mapping)
 
 
 if __name__ == "__main__":
